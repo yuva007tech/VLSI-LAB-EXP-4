13174968646d611301468271e0cb867e7b4daee3
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-05-12 21:47:57 +0530

    Update Exp4_Tflipflop.v

--- Exp4_Tflipflop.v
+++ Exp4_Tflipflop.v
@@ -1,8 +1,13 @@
-6(iv):T FLIPFLOP 
-module tff(t,clk,q); 
-input t,clk; 
-output reg q=1; 
-
-
-
+module T_flipflop(clk,rst,t,q);
+input clk,rst,t;
+output reg q;
+always @(posedge clk)
+begin
+if (rst==1)
+q=1'b0;
+else if (t==0)
+q=q;
+else
+q=~q;
+end
 endmodule
