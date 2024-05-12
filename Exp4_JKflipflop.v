65dad8a7d54892c456ba533abafb5fa7f944485f
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-05-12 21:45:27 +0530

    Update Exp4_JKflipflop.v

--- Exp4_JKflipflop.v
+++ Exp4_JKflipflop.v
@@ -1,8 +1,20 @@
-6(ii): JK FLIP FLOP 
-module jk( j,k,clk,q); 
-input j,k,clk; 
-output reg q=0; 
-
-
-
+module JK_flipflop (q, q_bar, j,k, clk,reset);
+input j,k,clk, reset;
+output reg q;
+output q_bar;
+always@(posedge clk)
+begin
+  if(!reset)        
+    q <= 0;
+  else 
+  begin
+      case({j,k})
+        2'b00: q <= q;    // No change
+        2'b01: q <= 1'b0; // reset
+        2'b10: q <= 1'b1; // set
+        2'b11: q <= ~q; // Toggle
+      endcase
+  end
+end
+assign q_bar = ~q;
 endmodule
