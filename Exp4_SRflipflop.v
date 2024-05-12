02a09bb332ae554cde3e9d86d57ed47b0a9acf06
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-05-12 21:47:27 +0530

    Update Exp4_SRflipflop.v

--- Exp4_SRflipflop.v
+++ Exp4_SRflipflop.v
@@ -1,13 +1,18 @@
-module sr( s,r,clk,q);
- input s,r,clk; 
-output reg q=0; 
-
-
-
-
+module sr_flipflop(s,r,clk,rst,q);
+input s,r,clk,rst;
+output reg q;
+always@(posedge clk)
+begin
+if(rst==1)
+q=0;
+else
+begin
+case({s,r})
+2'b00:q=q;
+2'b01:q=0;
+2'b10:q=1;
+2'b11:q=1'bX;
+endcase
+end
+end
 endmodule
-
-
-
-
-
