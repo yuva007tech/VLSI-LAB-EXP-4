6074a6130db29ad18a32c5dae3e50b8a87aa3e48
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-05-12 21:44:42 +0530

    Update Exp4_Dflipflop.v

--- Exp4_Dflipflop.v
+++ Exp4_Dflipflop.v
@@ -6,3 +6,16 @@ output reg q;
 
 
 endmodule
+module Dflipflop(D,clk,reset,Q);
+input D;
+input clk;
+input reset;
+output reg Q;
+always @(posedge clk)
+begin
+ if(reset==1'b1)
+  Q <= 1'b0;
+ else
+  Q <= D;
+end
+endmodule
